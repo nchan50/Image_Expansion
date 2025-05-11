5fe70f7538799ba57d97d469e262de8168f6868b
Nelson Chan <nchan50@stuy.edu>
2025-05-10 21:36:51 -0400

    Added get_USV to retrieve Database

--- Image_Expansion.py
+++ Image_Expansion.py
@@ -18,28 +18,40 @@ def img_SVD(file, channel):
       V.append(v[c, :])
   return np.array(U), np.array(S), np.array(V) # Turn list into arrays
       
-def store_USV(U, S, V, dir, name):
-  path = 'Database\\' + dir
-  os.makedirs(path, exist_ok=True) # Creates directory
-  if not os.path.exists(path + '\\' + name + '.npz') or WRITE_OVER_FILES:
-    np.savez(path + '\\' + name, U_data = U, S_data = S, V_data = V) # Saves the U, S, and V arrays in a .npz file
+def store_USV(U, S, V, directory, name):
+  path = f'Database\\{directory}'
+  os.makedirs(path, exist_ok=True) # Creates directoryectory
+  if not os.path.exists(f'{path}\\{name}.npz') or WRITE_OVER_FILES:
+    np.savez(f'{path}\\{name}', U = U, S = S, V = V) # Saves the U, S, and V arrays in a .npz file
   else:
-    print(path + '\\' + name + '.npz' + " already exists.")
+    print(f'{path}\\{name}.npz already exists.')
 
-def store_string(arr, dir, name):
-  path = 'Database\\' + dir + "\\str"
-  os.makedirs(path, exist_ok=True) # Creates directory
-  if not os.path.exists(path + '\\' + name) or WRITE_OVER_FILES:
-    np.savetxt(path + "\\" + name, arr, fmt='%.2f') # Saves an array in a text file
+def store_string(arr, directory, name):
+  path = f'Database\\{directory}\\str'
+  os.makedirs(path, exist_ok=True) # Creates directoryectory
+  if not os.path.exists(f'{path}\\{name}') or WRITE_OVER_FILES:
+    np.savetxt(f'{path}\\{name}', arr, fmt='%.2f') # Saves an array in a text file
   else:
-    print(path + '\\' + name + " already exists.")
+    print(f'{path}\\{name} already exists.')
+    
+def get_USV(name, channel, usv = 'USV'):
+  # usv: Letters determine which arrays are returned 
+  file = f'Database\\{name}\\{channel}.npz'
+  if os.path.isfile(file):
+    arrs = list()
+    for i in ['U', 'S', 'V']:
+      if i in usv:
+        arrs.append(np.load(file)[i])
+    return tuple(arrs)
+  else:
+    print(f'The file {file} does not exist.')
   
 def create_data(file, c = 'BGR', usv = True, string = False):
   # c : Letters determine the channels selected
   # usv : Determines if USV values are stored as a .npz file
   # str : Determines if USV values are stored as a text file
   name = file[:file.index('.')]
-  path = 'Patterns\\' + file
+  path = f'Patterns\\{file}'
   for channel in [BLUE, GREEN, RED]:
     color = BGR[channel]
     if color[0] in c:
@@ -47,9 +59,11 @@ def create_data(file, c = 'BGR', usv = True, string = False):
       if usv: 
         store_USV(U, S, V, name, color)
       if string: 
-        store_string(U, name, color + '_U')
-        store_string(S, name, color + '_S')
-        store_string(V, name, color + '_V')
+        store_string(U, name, f'{color}_U')
+        store_string(S, name, f'{color}_S')
+        store_string(V, name, f'{color}_V')
         
 create_data('pattern_1.png', c = 'RGB', string = True)
 create_data('pattern_2.png', c = 'RGB', string = True)
+U, S, V = get_USV('pattern_2', 'GREEN')
+print(S)
